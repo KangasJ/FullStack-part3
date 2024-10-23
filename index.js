a95e9539b8e2981c1557ffa0b7834b1fdf7e3bce
KangasJ <jani.2.kangas@student.hamk.fi>
2024-10-23 17:30:31 +0300

    New build

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 var morgan = require('morgan')
 app.use(express.json())
-
+app.use(express.static('dist'))
 let persons = [
     {
         id: "1",
