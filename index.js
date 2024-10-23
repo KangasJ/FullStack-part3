0762f3752d951b1ad19aa53ecabee51ef11d5005
KangasJ <jani.2.kangas@student.hamk.fi>
2024-10-23 16:29:15 +0300

    Tehty osan 3 tehtävät .1-8

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const app = express()
+var morgan = require('morgan')
+app.use(express.json())
+
+let persons = [
+    {
+        id: "1",
+        name:"Arto Hellas",
+        number:"040-123456"
+    },
+    {
+        id:"2",
+        name:"Ada Lovelace",
+        number:"39-44-5323523"
+    },
+    {
+        id:"3",
+        name:"Dan Abramov",
+        number:"12-43-234345"
+    },
+    {
+        id:"4",
+        name:"Mary Poppendieck",
+        number:"39-23-6423122"
+    }
+]
+
+morgan.token('data', function (req, res) { return JSON.stringify(req.body) }) // tehtävä 3.8
+app.use(morgan(':method :url :status :res[content-lenght] - :response-time ms  :data'))
+
+app.use(morgan('tiny')) //tehtävä 3.7
+
+app.get('/api/persons', (request, response) => { //tehtävä 3.1
+    response.json(persons)
+})
+app.get('/info', (request, response) => { //tehtävä 3.2
+    const numberOfPersons = persons.length
+    const currentTime = new Date()
+    
+    response.send(
+      `<p>Phonebook has numbers for ${numberOfPersons} people</p>
+       <p>${currentTime}</p>`
+    )
+})
+app.get('/api/persons/:id', (request, response) => { //tehtävä 3.3
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => { //tehtävä 3.4
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+
+const generateId = () => { //chatGPTn tarjoamana 
+    let newId
+    do {
+      newId = Math.floor(Math.random() * 10000) // Generoi satunnainen numero
+    } while (persons.some(person => person.id === String(newId))) // Tarkista, ettei ID ole jo käytössä
+    return String(newId) // Palauta ID merkkijonona
+  }
+
+app.post('/api/persons', (request, response) => { // tehtävä 3.5 ja 3.6
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const personExists = persons.some(person => person.name === body.name)
+    if (personExists) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
